f35b117a25945ffce637c2d98dbb9a909c1df7b7
Michael Wittig <witmic1@gmail.com>
2016-02-28 15:41:53 +0100

    more passing test for ringbuffer

--- ringbuffer/RingBuffer.py
+++ ringbuffer/RingBuffer.py
@@ -3,19 +3,20 @@ class RingBuffer:
         self.size = size
         self.items = {}
         self.read_index = 0
-        self.add_index = 0
+        self.write_index = 0
 
     def add(self, item):
-        if self.add_index == self.size:
-            self.add_index = 0
-            self.read_index += 1
-        self.items[self.add_index] = item
-        self.add_index += 1
+        if self.read_index in self.items.keys():
+            if self.write_index == self.read_index and not (self.items[self.read_index] is None):
+                self.read_index = self.increment_modulo(self.read_index)
+        self.items[self.write_index] = item
+        self.write_index = self.increment_modulo(self.write_index)
 
     def get(self):
-        if self.read_index == self.size:
-            self.read_index = 0
         ret = self.items[self.read_index]
         self.items[self.read_index] = None
-        self.read_index += 1
+        self.read_index = self.increment_modulo(self.read_index)
         return ret
+
+    def increment_modulo(self, index):
+        return (index + 1) % self.size
